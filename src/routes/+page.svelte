7573008c41bf2383ff576207c1b6c17d75d5d8e4
1yne <tnambiar2008@gmail.com>
2024-07-17 18:05:43 +0530

    Add gradient background to button on hover

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,5 +4,5 @@
 
 <div class="w-full flex flex-col items-center gap-12">
   <h1 class="font-playfair font-thin italic text-8xl">Lead The Way</h1>
-  <Button outline color="red" size="xl" class="border-red hover:bg-gradient-to-br">Vote Now</Button>
+  <Button outline color="red" size="xl" class="border-red hover:border-transparent hover:bg-gradient-to-br from-red to-purple">Vote Now</Button>
 </div>
