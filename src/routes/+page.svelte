c10782bd2fa67ecae9431fefc1b3543c42c9505a
1yne <tnambiar2008@gmail.com>
2024-07-17 17:58:23 +0530

    Add button to hero screen

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,8 @@
-<h1 class="font-playfair font-thin italic text-6xl">Lead The Way</h1>
+<script lang="ts">
+  import { Button } from 'flowbite-svelte';
+</script>
+
+<div class="w-full flex flex-col items-center gap-12">
+  <h1 class="font-playfair font-thin italic text-8xl">Lead The Way</h1>
+  <Button outline color="red" size="xl" class="border-red hover:bg-gradient-to-br">Vote Now</Button>
+</div>
